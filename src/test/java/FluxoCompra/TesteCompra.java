4a2919fa80279393e4ff584f0e5ea1a61e751865
diegopereira97 <diego.dick97@gmail.com>
2021-04-11 15:15:29 -0300

    #3

--- src/test/java/FluxoCompra/TesteCompra.java
+++ src/test/java/FluxoCompra/TesteCompra.java
@@ -1,6 +1,8 @@
 package FluxoCompra;
 
 import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -13,7 +15,7 @@ public class TesteCompra {
 
     private WebDriver navegador;
 
-    @Test
+    @Before
     public void Login () {
      System.setProperty("webdriver.chrome.driver", "D:\\Estudos\\Automação\\Drivers\\chromedriver.exe");
      navegador = new ChromeDriver();
@@ -26,6 +28,37 @@ public class TesteCompra {
         WebElement LoginEmail = navegador.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div[1]/form/div/div/div"));
         LoginEmail.findElement(By.name("email")).sendKeys("diego.dick97@gmail.com");
         LoginEmail.findElement(By.id("continue")).click();
+        WebElement LoginSenha = navegador.findElement(By.id("a-page"));
+        LoginSenha.findElement(By.id("ap_password")).sendKeys("toloveru123");
+        LoginSenha.findElement(By.id("signInSubmit")).submit();
+        WebElement Home = navegador.findElement(By.id("nav-link-accountList-nav-line-1"));
+        String Usuario = Home.getText();
+        Assert.assertEquals("Olá, diego", "Olá, diego");
+    }
+
+    @Test
+    public void testListaDestaque (){
+        WebElement EscolherProduto = navegador.findElement(By.xpath("//*[@id=\"nav-main\"]/div[1]"));
+        EscolherProduto.findElement(By.id("nav-hamburger-menu")).click();
+        WebElement Destaques = navegador.findElement(By.xpath("//*[@id=\"hmenu-content\"]/ul[1]/li[1]/div"));
+        Destaques.findElement(By.xpath("//*[@id=\"hmenu-content\"]/ul[1]/li[2]")).click();
+        //Destaques.findElement(By.xpath("//*[@id=\"hmenu-content\"]/ul[1]/li[3]"));
+        //Destaques.findElement(By.xpath("//*[@id=\"hmenu-content\"]/ul[1]/li[4]"));
+        WebElement MaisVendidos = navegador.findElement(By.id("zg_col1"));
+        String Maisvendidos = MaisVendidos.getText();
+        Assert.assertEquals("Mais vendidos", "Mais vendidos");
+        WebElement QualquerDepartamento = navegador.findElement(By.id("zg_browseRoot"));
+        QualquerDepartamento.findElement(By.cssSelector("#zg_browseRoot > ul > li:nth-child(14) > a")).click();
+        WebElement Eletronicos = navegador.findElement(By.id("zg-right-col"));
+        String ListaEletronicos = Eletronicos.getText();
+        Assert.assertEquals("Mais Vendidos em Eletrônicos", "Mais Vendidos em Eletrônicos");
+        Eletronicos.findElement(By.cssSelector("#zg-ordered-list > li:nth-child(1) > span > div > span")).click();
+        WebElement Eletronico1 = navegador.findElement(By.id("productTitle"));
+        String NomedoEletronico = Eletronico1.getText();
+        Assert.assertEquals("Novo Echo Dot (4ª Geração): Smart Speaker com Alexa - Cor Preta", "Novo Echo Dot (4ª Geração): Smart Speaker com Alexa - Cor Preta");
+        //MaisVendidos.findElement(By.xpath("//*[@id=\"zg_left_col1\"]/div[1]/div[1]/a"));
+
+
 
     }
 
